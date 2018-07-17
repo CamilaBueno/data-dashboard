d318d0eb6e6b30328b1204d74a33a9f2cc4d1c8f
Camila Bueno <cbf.camila@gmail.com>
2018-07-17 20:31:30 -0300

    Seleção de sede e turma

--- js/alunas.js
+++ js/alunas.js
@@ -1,20 +1,20 @@
-var dropMenu = document.getElementById("drop-menu");
-dropMenu.addEventListener("change", carregaProgramadoras);
+var dropMenu = document.getElementById('drop-menu');
+dropMenu.addEventListener('change', carregaProgramadoras);
 
-var dropTurma = document.getElementById("drop-turma");
-dropTurma.addEventListener("change", carregaTurma);
+var dropTurma = document.getElementById('drop-turma');
+dropTurma.addEventListener('change', carregaTurma);
 
 window.onload = carregaMenu();
 
 
 
 function carregaMenu() {
-  var nome = document.createElement("option");
-  nome.innerHTML = "Selecione sede";
-  nome.value = "none";
+  var nome = document.createElement('option');
+  nome.innerHTML = 'Selecione sede';
+  nome.value = 'none';
   dropMenu.appendChild(nome);
   for (sede in data) {
-    var itemMenu = document.createElement("option");
+    var itemMenu = document.createElement('option');
     itemMenu.value = sede;
     itemMenu.innerHTML = sede;
     dropMenu.appendChild(itemMenu);
@@ -22,28 +22,48 @@ function carregaMenu() {
   }
 };
 
+function carregaMenuTurma(turmas){
+  var nome = document.createElement('option');
+  dropTurma.appendChild(nome);
+  dropTurma.innerHTML = '';
+  nome.innerHTML = 'Selecione turma';
+  nome.value = 'none';
+  dropTurma.appendChild(nome);
+  for (turma in turmas) {
+    var itemMenu = document.createElement('option');
+    itemMenu.value = turma;
+    itemMenu.innerHTML = turma;
+    dropTurma.appendChild(itemMenu);
+  }
+};
+
 function carregaProgramadoras() {
   var sede = dropMenu.value;
-  var listaProgramadoras = document.getElementById("exibe-programadoras");
-  listaProgramadoras.innerHTML = "";
+  var listaProgramadoras = document.getElementById('exibe-programadoras');
+  listaProgramadoras.innerHTML = '';
   for (turma in data[dropMenu.value]) {
-    for (i in data[dropMenu.value][turma]["students"]) {
-      var img = document.createElement("img");
-      img.src = data[dropMenu.value][turma]["students"][i]["photo"];
+    for (i in data[dropMenu.value][turma]['students']) {
+      var img = document.createElement('img');
+      img.src = data[dropMenu.value][turma]['students'][i]['photo'];
       listaProgramadoras.appendChild(img);
     }
   }
+  carregaMenuTurma(data[dropMenu.value]);
 }
+
 function carregaTurma() {
   var sede = dropMenu.value;
   var turma = dropTurma.value;
-  var listaTurma = document.getElementById("exibe-turma");
-  listaTurma.innerHTML = "";
-  for(turma in data[dropMenu.value]) {
-    for(i in data[dropMenu.value][dropTurma.value]["students"]){
-      var geração = document.createElement("geração");
-      geração.src = data[dropMenu.value][dropTurma.value]["students"][i]["photo"];
-      listaTurma.appendChild(geração);
-    }
+  var listaProgramadoras = document.getElementById('exibe-programadoras');
+  listaProgramadoras.innerHTML = '';
+if(turma === 'none') {
+  carregaProgramadoras();
+} else{
+  for(i in data[sede][turma]['students']){
+    var geração = document.createElement('img');
+    geração.src = data[sede][turma]['students'][i]['photo'];
+    listaProgramadoras.appendChild(geração);
   }
+}
+    
 }
